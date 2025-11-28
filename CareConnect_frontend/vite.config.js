5c6a31f94ef976f4e622ec19b48a46f35d5d85ec
SageWaweru <sagewaweru2005@gmail.com>
2025-11-28 19:41:52 +0300

    fix MIME type errors

--- CareConnect_frontend/vite.config.js
+++ CareConnect_frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://careconnect-1-aayd.onrender.com', 
+        changeOrigin: true,
+        secure: false, 
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static',
+    manifest: true,
+  },
+});
